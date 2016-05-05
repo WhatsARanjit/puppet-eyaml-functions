2aea9daa1c113d9b7aed7bac50fab7c101eb798b
WhatsARanjit <whatsaranjit@gmail.com>
2016-05-04 23:35:04 -0400

    Added spec framework for eyaml_file tests

---
+++ spec/functions/eyaml_file_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'eyaml_file' do
+
+  let :secret do
+    'ENC[PKCS7,MIIBeQYJKoZIhvcNAQcDoIIBajCCAWYCAQAxggEhMIIBHQIBADAFMAACAQEwDQYJKoZIhvcNAQEBBQAEggEA0G2Cl2yCPlMsXkj3KOzMYO+kyXdSIoVad533Gr1FQaU7VqpM+rbt7CYM5WytFNYotsYyqxoSW3pGKKGxHL9f0c4q+xGZHV1GaHoR4rCnTps6fpyWVENkQJPZoEKi/fHSN2y+TryCf7Bt+3WRumkspZtUiZ0sG5G0kC80ssrDbbm2mSxwcZ1AvfEozcrCtCHJr3qxmFMEkHvhpW/roumuyhlgwhre6tkE7gqyntFrAR5Hzlt0Mk83Fg345qyKh/aq3iAv412/GgbKhFlHtMDcfW2y5ntZozuyvG7W73HQ64snRNR9JLUzZuhm0o/YQfUYFoSQIQIdi/TxCXb6nIZ46DA8BgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBD5w0KZx5jxul3GUbSUXpvDgBCO/6XLdwlHb16bagi0igXf]'
+  end
+
+  it { is_expected.not_to eq(nil) }
+  it { is_expected.to run.with_params().and_raise_error(ArgumentError, /wrong number of arguments/i) }
+  it { is_expected.to run.with_params(1,2).and_raise_error(ArgumentError, /wrong number of arguments/i) }
+
+  context 'using a file at /tmp/encrypted' do
+    before(:each) do
+      MockFunction.new('file') { |f|
+        f.stubs(:call).with(['/tmp/encrypted']).returns(secret)
+      }
+    end
+
+    it { is_expected.to run.with_params('/tmp/encrypted').and_return('hello') }
+  end
+
+  context 'using a file encrypted in module' do
+    before(:each) do
+      MockFunction.new('file') { |f|
+        f.stubs(:call).with(['foo/encrypted']).returns(secret)
+      }
+    end
+
+    it { is_expected.to run.with_params('foo/encrypted').and_return('hello') }
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'rspec-puppet-utils'
+require 'hiera/backend/eyaml'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
